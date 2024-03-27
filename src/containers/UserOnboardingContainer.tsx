d9f61fb8938e3c3ea92ea2b5a75838fc31ccb43d
Mark Erikson <mark@isquaredsoftware.com>
2024-03-27 15:59:51 -0400

    Force onboarding test failure

--- src/containers/UserOnboardingContainer.tsx
+++ src/containers/UserOnboardingContainer.tsx
@@ -69,71 +69,74 @@ const UserOnboardingContainer: React.FC<Props> = ({ authService, bankAccountsSer
     nextStep();
   };
 
-  return (
-    <Dialog data-test="user-onboarding-dialog" fullScreen={fullScreen} open={dialogIsOpen}>
-      <DialogTitle data-test="user-onboarding-dialog-title">
-        {userOnboardingState.matches("stepOne") && "Get Started with Real World App"}
-        {userOnboardingState.matches("stepTwo") && "Create Bank Account"}
-        {userOnboardingState.matches("stepThree") && "Finished"}
-      </DialogTitle>
-      <DialogContent data-test="user-onboarding-dialog-content">
-        <Box display="flex" alignItems="center" justifyContent="center">
-          {userOnboardingState.matches("stepOne") && (
-            <>
-              <NavigatorIllustration />
-              <br />
-              <DialogContentText style={{ paddingLeft: 20 }}>
-                Real World App requires a Bank Account to perform transactions.
-                <br />
-                <br />
-                Click <b>Next</b> to begin setup of your Bank Account.
-              </DialogContentText>
-            </>
-          )}
-          {userOnboardingState.matches("stepTwo") && (
-            <BankAccountForm
-              userId={currentUser?.id!}
-              createBankAccount={createBankAccountWithNextStep}
-              onboarding
-            />
-          )}
-          {userOnboardingState.matches("stepThree") && (
-            <>
-              <PersonalFinance />
-              <br />
-              <DialogContentText style={{ paddingLeft: 20 }}>
-                You're all set!
-                <br />
-                <br />
-                We're excited to have you aboard the Real World App!
-              </DialogContentText>
-            </>
-          )}
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Grid container justify="space-between">
-          <Grid item>
-            <Button
-              style={{ paddingRight: "80%" }}
-              onClick={/* istanbul ignore next */ () => sendAuth("LOGOUT")}
-              color="secondary"
-              data-test="user-onboarding-logout"
-            >
-              Logout
-            </Button>
-          </Grid>
-          <Grid item>
-            {!userOnboardingState.matches("stepTwo") && (
-              <Button onClick={() => nextStep()} color="primary" data-test="user-onboarding-next">
-                {userOnboardingState.matches("stepThree") ? "Done" : "Next"}
-              </Button>
-            )}
-          </Grid>
-        </Grid>
-      </DialogActions>
-    </Dialog>
-  );
+  return null;
+
+
+  // return (
+  //   <Dialog data-test="user-onboarding-dialog" fullScreen={fullScreen} open={dialogIsOpen}>
+  //     <DialogTitle data-test="user-onboarding-dialog-title">
+  //       {userOnboardingState.matches("stepOne") && "Get Started with Real World App"}
+  //       {userOnboardingState.matches("stepTwo") && "Create Bank Account"}
+  //       {userOnboardingState.matches("stepThree") && "Finished"}
+  //     </DialogTitle>
+  //     <DialogContent data-test="user-onboarding-dialog-content">
+  //       <Box display="flex" alignItems="center" justifyContent="center">
+  //         {userOnboardingState.matches("stepOne") && (
+  //           <>
+  //             <NavigatorIllustration />
+  //             <br />
+  //             <DialogContentText style={{ paddingLeft: 20 }}>
+  //               Real World App requires a Bank Account to perform transactions.
+  //               <br />
+  //               <br />
+  //               Click <b>Next</b> to begin setup of your Bank Account.
+  //             </DialogContentText>
+  //           </>
+  //         )}
+  //         {userOnboardingState.matches("stepTwo") && (
+  //           <BankAccountForm
+  //             userId={currentUser?.id!}
+  //             createBankAccount={createBankAccountWithNextStep}
+  //             onboarding
+  //           />
+  //         )}
+  //         {userOnboardingState.matches("stepThree") && (
+  //           <>
+  //             <PersonalFinance />
+  //             <br />
+  //             <DialogContentText style={{ paddingLeft: 20 }}>
+  //               You're all set!
+  //               <br />
+  //               <br />
+  //               We're excited to have you aboard the Real World App!
+  //             </DialogContentText>
+  //           </>
+  //         )}
+  //       </Box>
+  //     </DialogContent>
+  //     <DialogActions>
+  //       <Grid container justify="space-between">
+  //         <Grid item>
+  //           <Button
+  //             style={{ paddingRight: "80%" }}
+  //             onClick={/* istanbul ignore next */ () => sendAuth("LOGOUT")}
+  //             color="secondary"
+  //             data-test="user-onboarding-logout"
+  //           >
+  //             Logout
+  //           </Button>
+  //         </Grid>
+  //         <Grid item>
+  //           {!userOnboardingState.matches("stepTwo") && (
+  //             <Button onClick={() => nextStep()} color="primary" data-test="user-onboarding-next">
+  //               {userOnboardingState.matches("stepThree") ? "Done" : "Next"}
+  //             </Button>
+  //           )}
+  //         </Grid>
+  //       </Grid>
+  //     </DialogActions>
+  //   </Dialog>
+  // );
 };
 
 export default UserOnboardingContainer;
