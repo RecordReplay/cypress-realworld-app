3f404a8d3ab59d6bb4f12715c40e7dd6706d1fe7
Mark Erikson <mark@isquaredsoftware.com>
2024-03-27 14:44:41 -0400

    Second bump

--- cypress/tests/ui/auth.spec.ts
+++ cypress/tests/ui/auth.spec.ts
@@ -1,7 +1,7 @@
 import { User } from "../../../src/models";
 import { isMobile } from "../../support/utils";
 
-// useless comment
+// second useless comment
 
 const apiGraphQL = `${Cypress.env("apiUrl")}/graphql`;
 
