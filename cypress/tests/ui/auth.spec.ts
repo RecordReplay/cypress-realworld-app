fdd49e1e1faedcc33a688793c694657707c97835
Mark Erikson <mark@isquaredsoftware.com>
2024-05-01 15:06:24 -0400

    Twenty-fifth bump

--- cypress/tests/ui/auth.spec.ts
+++ cypress/tests/ui/auth.spec.ts
@@ -1,7 +1,7 @@
 import { User } from "../../../src/models";
 import { isMobile } from "../../support/utils";
 
-// Twenty-fourth useless comment
+// Twenty-fifth useless comment
 
 const apiGraphQL = `${Cypress.env("apiUrl")}/graphql`;
 
