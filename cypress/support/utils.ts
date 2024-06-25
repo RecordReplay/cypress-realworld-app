d4846056fedc1e7ce0de2782b17b691c3771899a
Mark Erikson <mark@isquaredsoftware.com>
2024-06-25 15:00:22 -0400

    Disable mobile check

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,3 +1,4 @@
 export const isMobile = () => {
-  return Cypress.config("viewportWidth") < Cypress.env("mobileViewportWidthBreakpoint");
+  return false;
+  // return Cypress.config("viewportWidth") < Cypress.env("mobileViewportWidthBreakpoint");
 };
