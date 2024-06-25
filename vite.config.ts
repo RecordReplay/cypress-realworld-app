fafecb30cc88bcd3dccdd5228edc9feba10bc001
Mark Erikson <mark@isquaredsoftware.com>
2024-06-25 15:44:57 -0400

    Add sourcemaps for React 18.2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 import istanbul from "vite-plugin-istanbul";
+import { ViteReactSourcemapsPlugin } from "@acemarke/react-prod-sourcemaps";
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE");
@@ -26,6 +27,7 @@ export default defineConfig(({ command, mode }) => {
         exclude: ["node_modules", "cypress", "dist"],
         forceBuildInstrument: true,
       }),
+      ViteReactSourcemapsPlugin({ debug: false, preserve: false }),
     ],
     // to get aws amplify to work with vite
     resolve: {
