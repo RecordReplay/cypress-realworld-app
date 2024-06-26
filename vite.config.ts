eddfe9934260c45de6db7e620405d407c939d3b9
Mark Erikson <mark@isquaredsoftware.com>
2024-06-25 21:32:35 -0400

    Include lib sourcemaps in bundle

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 import istanbul from "vite-plugin-istanbul";
+import sourcemaps from "rollup-plugin-sourcemaps";
 import { ViteReactSourcemapsPlugin } from "@acemarke/react-prod-sourcemaps";
 
 export default defineConfig(({ command, mode }) => {
@@ -27,6 +28,7 @@ export default defineConfig(({ command, mode }) => {
         exclude: ["node_modules", "cypress", "dist"],
         forceBuildInstrument: true,
       }),
+      sourcemaps(),
       ViteReactSourcemapsPlugin({ debug: false, preserve: false }),
     ],
     // to get aws amplify to work with vite
